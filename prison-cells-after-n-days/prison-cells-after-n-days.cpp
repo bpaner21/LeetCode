7100c24f7af438c002c49d1aec23b3a636abf732
BJ Paner <bernardjohn.paner@gmail.com>
2021-10-07 18:34:10 -0500

    Time: 4 ms (81.24%), Space: 12 MB (82.28%) - LeetHub

---
+++ prison-cells-after-n-days/prison-cells-after-n-days.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int n) {
+        
+        // Because this problem can go up to 10^9 days
+        // rather than calculating the state of each day manually,
+        // it is much more time efficient to attempt to find a cycle
+        // between the states of cells 1 through cells.size() - 2
+        //
+        // This is because any given state can only become one other state the next day
+        // and there are only 14 permutations for the states of cells 1 through cells.size() - 2
+        //
+        // Due to the rules, cells[0] and cells[cells.size() - 1] will always become 0 by the second day,
+        // thus they can be ignored when trying to find a cycle between states
+        
+        vector<int> temp(8, 0);
+        
+        // Find the number of states to loop through by 
+        // calculating how many times the cells would cycle through states 1 - 14
+        // and find the remaining number of days left
+        //
+        // because the value of both the first and last cells may begin at 1, the loop will cycle between 1 - 14 times
+        // to ensure that they are they are properly set to 0
+        n = ((n - 1) % 14) + 1;
+        
+        for (n; n > 0; --n) {
+            
+            for (int i = 1; i < 7; ++i) {
+                
+                temp[i] = (cells[i - 1] == cells[i + 1]) ? 1 : 0;
+            }
+            
+            cells = temp;
+        }
+        
+        return cells;
+    }
+};
