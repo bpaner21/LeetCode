b4260c63b25cca23120392dead1e10cf133a9506
BJ Paner <bernardjohn.paner@gmail.com>
2021-10-09 16:32:32 -0500

    Time: 0 ms (100.00%), Space: 37.4 MB (57.46%) - LeetHub

--- custom-sort-string/custom-sort-string.java
+++ custom-sort-string/custom-sort-string.java
@@ -1,8 +1,10 @@
 class Solution {
     public String customSortString(String order, String s) {
         
+        // StringBuilder has better performance than String
         StringBuilder answer = new StringBuilder();
         
+        // record the frequency of characters in s
         int frequency[] = new int[26];
         
         for (char i : s.toCharArray()) {
@@ -10,6 +12,9 @@ class Solution {
             ++frequency[i - 'a'];
         }
         
+        // iterating through the characters in the custom order,
+        // add them to the return string, if they appear in s,
+        // while removing them from the frequency array
         for (char j : order.toCharArray()) {
             
             while (frequency[j - 'a'] > 0) {
@@ -20,6 +25,8 @@ class Solution {
             }
         }
         
+        // add the remaining characters from s
+        // to the return string in alphabetical order
         for (char k = 'a'; k <= 'z'; ++k) {
             
             while (frequency[k - 'a'] > 0) {
