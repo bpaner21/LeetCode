3ae0966e04a04e7d069e8e1fcc6dc4e418b76375
BJ Paner <bernardjohn.paner@gmail.com>
2021-10-12 16:56:38 -0500

    Time: 0 ms (100.00%), Space: 5.9 MB (75.34%) - LeetHub

---
+++ bulb-switcher/bulb-switcher.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int bulbSwitch(int& n) {
+        
+        return sqrt(n);
+    }
+};
+
+/*
+    This is not a coding problem so much as it is a logic puzzle.
+    
+    Every bulb is initially off, but gets turned on so long as n >= 1,
+    
+    The bulbs that would be still be on after n rounds are those that have only an odd amount of factors,
+    
+    in otherwords, numbers that are perfect squares, between 0 and n, which can be easily calculated by the square root of n.
+    
+    * If n == 0, 0 is returned
+    
+    * If n == 1, 1 is returned since its only factor is itself
+    
+    * If n > 1, return the value of sqrt(n), rounding down to the nearest integer
+*/
