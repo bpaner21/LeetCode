b6b73d65ce306cccf8477d3c32372c2e632a2bff
BJ Paner <bernardjohn.paner@gmail.com>
2021-10-12 17:09:57 -0500

    Time: 0 ms (100.00%), Space: 5.7 MB (94.12%) - LeetHub

---
+++ bulb-switcher-ii/bulb-switcher-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int flipLights(int n, int presses) {
+        
+        // if (n == 0) { return 0; } // not possible given problem restraints
+        
+        if (presses == 0) { return 1; }
+        
+        if (n == 1) { return 2; }
+        
+        if (n == 2) {
+            
+            return presses == 1 ? 3 : 4;
+        }
+        
+        // 3 or more bulbs
+        
+        if (presses == 1) { return 4; }
+        
+        return presses == 2 ? 7 : 8;
+    }
+};
+
