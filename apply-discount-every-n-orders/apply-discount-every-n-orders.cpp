52aa943c8981d326fc10bf5420bf6b34f039326e
BJ Paner <bernardjohn.paner@gmail.com>
2021-10-18 16:25:20 -0500

    Time: 323 ms (34.03%), Space: 120.7 MB (53.93%) - LeetHub

--- apply-discount-every-n-orders/apply-discount-every-n-orders.cpp
+++ apply-discount-every-n-orders/apply-discount-every-n-orders.cpp
@@ -1,25 +1,26 @@
 class Cashier {
 private:
     
-    short int _count = 0;
-    short int _n = 0;
-    short int _discount = 0;
-    unordered_map<short int, short int> _productPrices {};
+    int _count = 0;
+    int _n = 0;
+    int _discount = 0;
+    unordered_map<int, int> _productPrices {};
     
 public:
-    Cashier(int n, int discount, const vector<int>& products, const vector<int>& prices) : _n(n), _discount(discount) {
+    Cashier(int& n, int& discount, vector<int>& products, vector<int>& prices) {
         
-        for (short int i = 0; i < products.size(); ++i) {
+        _n = n;
+        _discount = discount;
+        
+        for (int i = 0; i < products.size(); ++i) {
             
-            _productPrices.emplace(products[i], prices[i]);
+            _productPrices[products[i]] = prices[i];
         }
     }
     
     double getBill(const vector<int>& product, const vector<int>& amount) {
         
         ++_count;
-        
-        double discount = (_count % _n == 0) ? (100.0 - _discount) / 100.0 : 1.0;
         
         double bill = 0.0;
         
@@ -28,7 +29,13 @@ public:
             bill += _productPrices[product[i]] * amount[i];
         }
         
-        return bill * discount;
+        if (_count == _n) {
+            
+            _count = 0;
+            bill *= (100 - _discount) / 100.0;
+        }
+        
+        return bill;
     }
 };
 
