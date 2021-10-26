bd6ae1ebb78f6c981041d9b2b272c3e1f114bf29
BJ Paner <bernardjohn.paner@gmail.com>
2021-10-26 14:56:10 -0500

    Time: 2 ms (99.48%), Space: 37.7 MB (80.58%) - LeetHub

--- partition-labels/partition-labels.java
+++ partition-labels/partition-labels.java
@@ -1,33 +1,29 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         
-        char ch[] = s.toCharArray();
-        
         int lastIndex[] = new int[26];
-        
-        int size = ch.length;
         
         ArrayList<Integer> partitions = new ArrayList<>();
         
         // record the rightmost index of each letter in s
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < s.length(); ++i) {
             
-            lastIndex[ch[i] - 'a'] = i;
+            lastIndex[s.charAt(i) - 'a'] = i;
         }
         
         // using the record of the rightmost indices as end points,
         // create partitions that contain every appearnce of the letters they hold
-        for (int left = 0; left < size; ++left) {
+        for (int left = 0; left < s.length(); ++left) {
             
             int right = left;
             
-            int partitionEnd = lastIndex[ch[left] - 'a'];
+            int partitionEnd = lastIndex[s.charAt(left) - 'a'];
             
             while (left < partitionEnd) {
                 
                 ++left;
                 
-                partitionEnd = Math.max(partitionEnd, lastIndex[ch[left] - 'a']);
+                partitionEnd = Math.max(partitionEnd, lastIndex[s.charAt(left) - 'a']);
             }
             
             partitions.add(left - right + 1);
