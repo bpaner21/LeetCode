bcdcdeb63c7c4cd9c874c6a8a029304d0e3fac30
BJ Paner <bernardjohn.paner@gmail.com>
2021-11-05 15:38:36 -0500

    Time: 1 ms (85.93%), Space: 40.5 MB (57.05%) - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        // If nums has length 1 or less, then it is already within the accepted parameters
+        if (nums.length < 1) { return nums.length; }
+        
+        
+        int k = 0;
+        
+        for (int n : nums) {
+            
+            /*
+                [if (k < 1)] automatically sets num[0] equal to itself
+                
+                [if (nums[k - 1] != n)] checks if there is more than one instance of a unique element in the array
+                
+                [if (nums[k - 1] < n)] would also work but may be more difficult to intuit
+            */
+            
+            if (k < 1 || nums[k - 1] != n) {
+                
+                nums[k++] = n;
+            }
+        }
+        
+        
+        return k;
+    }
+}
