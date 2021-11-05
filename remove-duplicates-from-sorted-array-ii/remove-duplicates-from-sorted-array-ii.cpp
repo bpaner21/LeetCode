c7803371bd6dba3062ce37cbea278b24f2649e4e
BJ Paner <bernardjohn.paner@gmail.com>
2021-11-05 15:32:26 -0500

    Time: 4 ms (86.50%), Space: 10.9 MB (75.26%) - LeetHub

---
+++ remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        // If nums has length 2 or less, array is already within accepted boundaries
+        if (nums.size() < 3) { return nums.size(); }
+        
+        
+        int k = 0;
+        
+        for (int n : nums) {
+            
+            /*
+                [if (k < 2)] automatically sets num[0] and nums[1] equal to themselves
+                
+                [if (nums[k - 2] != n)] checks if there are more than two instances of a unique element in the array
+                
+                [if (nums[k - 2] < n)] would also work but may be more difficult to intuit
+            */
+            
+            if (k < 2 || nums[k - 2] != n) {
+                
+                nums[k++] = n;
+            }
+        }
+        
+        
+        return k;
+    }
+};
