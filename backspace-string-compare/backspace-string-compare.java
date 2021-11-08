da416ea0d0436fcb5140980b1fa43dbd80823a7f
BJ Paner <bernardjohn.paner@gmail.com>
2021-11-07 21:35:38 -0600

    Time: 0 ms (100.00%), Space: 37.4 MB (65.45%) - LeetHub

---
+++ backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,82 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        
+        // this can be solved with a stack,
+        // but that would not be O(1) space
+        
+        int i = s.length() - 1, j = t.length() - 1;
+        
+        int skipS = 0, skipT = 0;
+        
+        // loop backwards starting from the ends
+        // of strings s and t
+        while (i >= 0 || j >= 0)
+        {
+            // if the loop runs into a '#' symbol in s
+            // decrement its current index
+            // and increase its skip count
+            if (i >= 0 && s.charAt(i) == '#') {
+                
+                ++skipS;
+                
+                --i;
+                
+                continue;
+            }
+            
+            // if the skip count for s is greater than 0,
+            // decrement its index and skip count
+            // 
+            // these two if-statements together 
+            // simulate a backspace function in O(n) time and O(1) space
+            if (i >= 0 && skipS > 0) {
+                
+                --skipS;
+                
+                --i;
+                
+                continue;
+            }
+
+            
+            // backspace functions for t
+            if (j >= 0 && t.charAt(j) == '#') {   
+                
+                ++skipT;
+                
+                --j;
+                
+                continue;
+            }
+            
+            if (j >= 0 && skipT > 0) {
+                
+                --skipT;
+                
+                --j;
+                
+                continue;
+            }
+            
+            
+            // once the backspace functions have concluded
+            // i and j should point to the same character
+            // otherwise s and t are not equal strings
+            if (i >= 0 && j >= 0 && s.charAt(i) == t.charAt(j)) {
+                
+                --i;
+                
+                --j;
+            }
+            else {
+                
+                return false;
+            }
+        }
+        
+        // if the loop concluded and i != j,
+        // then s and t are different strings of unqual length
+        // (if s and t are equal, i = j = -1)
+        return (i == j); 
+    }
+}
