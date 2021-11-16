1e50ce7dc9f90c3529134c715de65b7049d18d0a
BJ Paner <bernardjohn.paner@gmail.com>
2021-11-16 00:19:47 -0600

    Time: 352 ms (57.46%), Space: 71.8 MB (44.22%) - LeetHub

--- check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,7 +4,7 @@ public:
         
         if (s.size() < k) { return false; }
         
-        set<std::string> codes {};
+        unordered_set<std::string> codes {};
         
         for (int i = 0; i < s.size() - k + 1; ++i) {
             
